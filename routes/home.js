73e2bce42ef96bbd044cecfb23f4dcbf45229c57
ndhuu <danghuu.dna@gmail.com>
2019-10-11 23:20:12 +0800

    home page

---
+++ routes/home.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+/* SQL Query */
+var sql_query = 'INSERT INTO student_info VALUES';
+
+// GET
+router.get('/', function(req, res, next) {
+	res.render('insert', { title: 'Modifying Database' });
+});
+
+// POST
+router.post('/', function(req, res, next) {
+	// Retrieve Information
+	var matric  = req.body.matric;
+	var name    = req.body.name;
+	var faculty = req.body.faculty;
+	
+	// Construct Specific SQL Query
+	var insert_query = sql_query + "('" + matric + "','" + name + "','" + faculty + "')";
+	
+	pool.query(insert_query, (err, data) => {
+		res.redirect('/select')
+	});
+});
+
+module.exports = router;
