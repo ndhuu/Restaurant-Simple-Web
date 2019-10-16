6f70ad6b259eb8b46cee6caa5c63bff8cee84d75
micklwl <micklwl@hotmail.com>
2019-10-17 00:56:55 +0800

    update sql table creation

--- db/postgresql_script.sql
+++ db/postgresql_script.sql
@@ -7,6 +7,7 @@ DROP TABLE IF EXISTS Rest_Location 	CASCADE;
 DROP TABLE IF EXISTS Rest_Cuisine 	CASCADE;
 DROP TABLE IF EXISTS Owner_Rest 	CASCADE;
 DROP TABLE IF EXISTS OpeningHours 	CASCADE;
+DROP TABLE IF EXISTS Availability 	CASCADE;
 DROP TABLE IF EXISTS Promotion 		CASCADE;
 DROP TABLE IF EXISTS Fnb 			CASCADE;
 DROP TABLE IF EXISTS Rewards 		CASCADE;
@@ -14,28 +15,46 @@ DROP TABLE IF EXISTS Locations 		CASCADE;
 DROP TABLE IF EXISTS Cuisines 		CASCADE;
 DROP TABLE IF EXISTS Restaurants 	CASCADE;
 DROP TABLE IF EXISTS Diners 		CASCADE;
+DROP TABLE IF EXISTS Workers 		CASCADE;
 DROP TABLE IF EXISTS Owners 		CASCADE;
+DROP TABLE IF EXISTS Company 		CASCADE;
+DROP TABLE IF EXISTS Admin 			CASCADE;
 DROP TABLE IF EXISTS Users 			CASCADE;
 
---user can be either owner,diner or both at this point of time
---add a type field to users, diners, owners
---Normal Entity Sets
+-- MySQL retrieves and displays DATE values in 'YYYY-MM-DD' format
+-- MySQL retrieves and displays TIME values in 'hh:mm:ss' format 
 CREATE TABLE Users(
 	name		varchar(255) 	NOT NULL,
-	phoneNum 	integer 		NOT NULL,
-	email 		varchar(255) 	NOT NULL,
+	phoneNum 	varchar(8) 		NOT NULL,
+	email 		varchar(255) 	NOT NULL CHECK (email LIKE '%@%.%'),
+	uname 		varchar(255) 	PRIMARY KEY,
+	password	varchar(255) 	NOT NULL,
+	type		varchar(255) NOT NULL CHECK (type in ('Worker','Owner','Diner'))
+);
+
+CREATE TABLE Admin (
 	uname 		varchar(255) 	PRIMARY KEY,
 	password	varchar(255) 	NOT NULL
 );
 
-CREATE TABLE Owners (
+CREATE TABLE Company (
 	uname 		varchar(255) 	PRIMARY KEY,
 	FOREIGN KEY (uname) REFERENCES Users(uname) ON DELETE cascade
 );
 
+CREATE TABLE Owners (
+	uname 		varchar(255) 	PRIMARY KEY,
+	FOREIGN KEY (uname) REFERENCES Company(uname) ON DELETE cascade
+);
+
+CREATE TABLE Workers (
+	uname 		varchar(255) 	PRIMARY KEY,
+	FOREIGN KEY (uname) REFERENCES Company(uname) ON DELETE cascade
+);
+
 CREATE TABLE Diners (
 	uname 		varchar(255) 	PRIMARY KEY,
-	points 		integer,
+	points 		integer DEFAULT '0' NOT NULL,
 	FOREIGN KEY (uname) REFERENCES Users(uname) ON DELETE cascade
 );
 
@@ -53,10 +72,25 @@ CREATE TABLE Locations (
 	area 		varchar(255) 	PRIMARY KEY
 );
 
+
 CREATE TABLE Rewards (
-	rID 		integer 		PRIMARY KEY,
+	rewardsCode	integer 		PRIMARY KEY,
 	pointsReq 	integer 		NOT NULL,
-	duration 	integer
+	s_date 		date 			NOT NULL,
+	e_date		date 			NOT NULL,
+	duration 	integer		DEFAULT '3' NOT NULL,
+	details 	varchar(255)
+);
+
+--Diner related
+CREATE TABLE Redemptions (
+	dname 		varchar(255) 	REFERENCES Diners(uname) ON DELETE CASCADE,
+	rewardsCode integer 	DEFAULT '0' REFERENCES Rewards(rewardsCode) ON DELETE SET DEFAULT,
+	s_date 		date			NOT NULL,
+	e_date		date			NOT NULL,
+	time 		time			NOT NULL,
+	is_valid	boolean			DEFAULT FALSE NOT NULL,
+	PRIMARY KEY (dname, rewardsCode)
 );
 
 --Weak Entity Sets
@@ -64,7 +98,7 @@ CREATE TABLE Fnb (
 	rname 		varchar(255),
 	address 	varchar(255),
     fname 		varchar(255),
-	price 		integer 		NOT NULL,
+	price 		numeric 		NOT NULL CHECK (price > 0),
 	PRIMARY KEY (rname, address, fname),
 	FOREIGN KEY (rname, address) REFERENCES Restaurants(rname, address) ON DELETE cascade
 );
@@ -73,22 +107,32 @@ CREATE TABLE Promotion (
 	rname 		varchar(255),
 	address 	varchar(255),
 	time 		time,
-	discount 	integer,
+	discount 	numeric CHECK (discount > 0),
 	PRIMARY KEY (rname, address, time, discount),
 	FOREIGN KEY (rname, address) REFERENCES Restaurants(rname, address) ON DELETE cascade
 );
 
---need create check for day and time
---Is day sufficient or do we consider the s_time, how would be know the s_time then?
+
 CREATE TABLE OpeningHours (
 	rname 		varchar(255),
 	address 	varchar(255),
-	day 		varchar(255),
-	s_time 		time 			NOT NULL,
-	hours 		integer 		NOT NULL,
+	day 		varchar(255) CHECK (day in ('Mon','Tues','Wed','Thurs','Fri','Sat','Sun')),
+	s_time 		time,
+	hours 		integer 		NOT NULL CHECK (hours > 0),
 	PRIMARY KEY(rname, address, day, s_time),
 	FOREIGN KEY (rname, address) REFERENCES Restaurants(rname, address) ON DELETE cascade
 );
+
+CREATE TABLE Availability (
+	rname 		varchar(255),
+	address 	varchar(255),
+	date 		date,
+	s_time 		time,
+	maxPax 		integer DEFAULT NULL CHECK (maxPax > 0),
+	PRIMARY KEY(rname, address, date, s_time),
+	FOREIGN KEY (rname, address) REFERENCES Restaurants(rname, address) ON DELETE cascade
+);
+
 
 --Relation Set
 --Restaurant related
@@ -118,37 +162,55 @@ CREATE TABLE Rest_Location (
 	FOREIGN KEY (rname, address) REFERENCES Restaurants(rname, address) ON DELETE cascade
 );
 
---Diner related
-CREATE TABLE Redemptions (
-	dname 		varchar(255) 	REFERENCES Diners(uname),
-	rID 		integer 		REFERENCES Rewards(rID),
-	date 		date			NOT NULL,
-	time 		time			NOT NULL,
-	PRIMARY KEY (dname, rID)
-);
-
 --Both related
 CREATE TABLE Favourites (
-	dname 		varchar(255) 	REFERENCES Diners(uname),
+	dname 		varchar(255) 	REFERENCES Diners(uname) ON DELETE cascade,
 	rname 		varchar(255),
 	address		varchar(255),
 	FOREIGN KEY (rname, address) REFERENCES Restaurants(rname, address) ON DELETE cascade,
 	PRIMARY KEY (dname, rname)
 );
 
---status needs to have check or type changed
+
 CREATE TABLE Reservations (
-	dname 		varchar(255) 	REFERENCES Diners(uname),
+	dname 		varchar(255) 	DEFAULT 'DEFAULT' REFERENCES Diners(uname) ON DELETE SET DEFAULT,
 	rname 		varchar(255),
 	address 	varchar(255),
-	numPax 		integer			NOT NULL,
-	time 		time			NOT NULL,
-	date 		date			NOT NULL,
-	status 		varchar(255)	NOT NULL,
+	numPax 		integer			NOT NULL CHECK (numPax > 0),
+	time 		time,
+	date 		date,
+	status 		varchar(255)	DEFAULT 'Pending' NOT NULL CHECK (status in ('Pending','Confirmed','Completed')),
 	rating 		integer,
 	PRIMARY KEY (dname, rname, address, time, date),
 	FOREIGN KEY (rname, address) REFERENCES Restaurants(rname, address) ON DELETE cascade
 );
+
+
+CREATE OR REPLACE FUNCTION t_func1() 
+RETURNS TRIGGER AS $$ BEGIN 
+RAISE NOTICE 'Trigger 1'; RETURN NULL; 
+END; $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trig1 
+BEFORE INSERT OR UPDATE ON Users 
+FOR EACH ROW 
+WHEN (NEW.uname = 'DEFAULT') 
+EXECUTE PROCEDURE t_func1();
+
+CREATE OR REPLACE FUNCTION t_func2() 
+RETURNS TRIGGER AS $$ BEGIN 
+RAISE NOTICE 'Trigger 2'; RETURN NULL; 
+END; $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trig2 
+BEFORE INSERT OR UPDATE ON Rewards 
+FOR EACH ROW 
+WHEN (NEW.rewardsCode = 0) 
+EXECUTE PROCEDURE t_func2();
+
+
+
+
 /*
 CREATE TABLE student_info (
 	matric  varchar(9) PRIMARY KEY,
