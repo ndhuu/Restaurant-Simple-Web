13f0c31036ad8ba8aa680e5969c77479fac7fe10
ndhuu <danghuu.dna@gmail.com>
2019-11-08 18:21:38 +0800

    add report

--- routes/my_restaurants_reser.js
+++ routes/my_restaurants_reser.js
@@ -11,9 +11,10 @@ function getSampleLink(rname, address) {
   rname = encodeURI(encodeHashtag(rname));
   address = encodeURI(encodeHashtag(address));
   var sampleLink = {
-    pending_page    : `/my_restaurants/reservation/pending:${rname}&:${address}`,
-    confirmed_page  : `/my_restaurants/reservation/confirmed:${rname}&:${address}`,
-    completed_page  : `/my_restaurants/reservation/completed:${rname}&:${address}`,
+    pending_page: `/my_restaurants/reservation/pending:${rname}&:${address}`,
+    confirmed_page: `/my_restaurants/reservation/confirmed:${rname}&:${address}`,
+    completed_page: `/my_restaurants/reservation/completed:${rname}&:${address}`,
+    report: `/my_restaurants/reservation/report`
   }
   return sampleLink;
 }
@@ -46,17 +47,17 @@ router.get('/pending:rname&:address', function (req, res, next) {
       for (let i = 0; i < data.rows.length; i++) {
         data.rows[i]["action"] = "/reservation/pending/accept"
       }
-      res.render('my_rest_reservations/pending', { title: 'My Restaurants', data: data.rows, rname, address, sample_link: getSampleLink(rname, address) });
+      res.render('my_rest_reservations/pending', { title: 'My Restaurants', data: data.rows, rname: rname, address: address, sample_link: getSampleLink(rname, address) });
     }
   });
 });
 
 router.post('/pending/accept', function (req, res, next) {
-  var rname = req.query.rname;
-  var dname = req.query.dname;
-  var address = req.query.address;
-  var time = req.query.time;
-  var date = req.query.date;
+  var rname = req.body.rname;
+  var dname = req.body.dname;
+  var address = req.body.address;
+  var time = req.body.time;
+  var date = req.body.date;
 
   console.log("Go to full my restaurant reservation page: pending")
   pool.query(sql_query.query.accept_reser, [dname, rname, address, time, date], (err, data) => {
@@ -81,19 +82,19 @@ router.get('/confirmed:rname&:address', function (req, res, next) {
       res.redirect('/my_restaurants')
     } else {
       for (let i = 0; i < data.rows.length; i++) {
-        data.rows[i]["action"] = "/reservation/pending/complete"
+        data.rows[i]["action"] = "/reservation/confirmed/complete"
       }
-      res.render('my_rest_reservations/confirmed', { title: 'My Restaurants', data: data.rows, rname, address, sample_link: getSampleLink(rname, address) });
+      res.render('my_rest_reservations/confirmed', { title: 'My Restaurants', data: data.rows, rname: rname, address: address, sample_link: getSampleLink(rname, address) });
     }
   });
 });
 
 router.post('/confirmed/complete', function (req, res, next) {
-  var rname = req.query.rname;
-  var dname = req.query.dname;
-  var address = req.query.address;
-  var time = req.query.time;
-  var date = req.query.date;
+  var rname = req.body.rname;
+  var dname = req.body.dname;
+  var address = req.body.address;
+  var time = req.body.time;
+  var date = req.body.date;
 
   console.log("Go to full my restaurant reservation page: pending")
   pool.query(sql_query.query.com_reser, [dname, rname, address, time, date], (err, data) => {
@@ -117,9 +118,43 @@ router.get('/completed:rname&:address', function (req, res, next) {
       console.error(err)
       res.redirect('/my_restaurants')
     } else {
-      res.render('my_rest_reservations/completed', { title: 'My Restaurants', data: data.rows, rname, address, sample_link: getSampleLink(rname, address) });
+      res.render('my_rest_reservations/completed', { title: 'My Restaurants', data: data.rows, rname: rname, address: address, sample_link: getSampleLink(rname, address) });
     }
   });
+});
+
+
+router.get('/report:rname&:address&:year', function (req, res, next) {
+  var rname = decodeURI(decodeHashtag(req.params.rname)).substr(1)
+  var address = decodeURI(decodeHashtag(req.params.address)).substr(1)
+  var year = decodeURI(decodeHashtag(req.params.year)).substr(1)
+  year = parseInt(year)
+
+
+  console.log("Go to full my restaurant reservation page: completed")
+  console.log(sql_query.query.view_restreport, [rname, address, year.rname, address, year])
+  pool.query(sql_query.query.view_restreport, [rname, address, year, rname, address, year], (err, data) => {
+    if (err) {
+      console.error("Error in my owner reservation completed page");
+      console.error(err)
+      res.redirect('/my_restaurants')
+    } else {
+      console.log(data.rows)
+
+      res.render('my_rest_reservations/report', { title: 'My Restaurants', data: data.rows, rname: rname, address: address, sample_link: getSampleLink(rname, address) });
+    }
+  });
+});
+
+router.post('/report', function (req, res, next) {
+  var rname = req.body.rname;
+  var address = req.body.address;
+  var year = (req.body.year);
+  console.log(rname);
+  console.log(address);
+  console.log(year);
+  res.redirect(`/my_restaurants/reservation/report:${encodeURI(encodeHashtag(rname))}&:${encodeURI(encodeHashtag(address))}&:${encodeURI(encodeHashtag(year))}`)
+
 });
 
 module.exports = router;
